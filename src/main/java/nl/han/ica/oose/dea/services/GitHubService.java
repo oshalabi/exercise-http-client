23c71ebfa591daa871e3b6c46a2e2ce7a4c4022b
Osama <oshalabi91@gmail.com>
2020-09-15 23:17:56 +0200

    Update GitHubService.java

--- src/main/java/nl/han/ica/oose/dea/services/GitHubService.java
+++ src/main/java/nl/han/ica/oose/dea/services/GitHubService.java
@@ -1,9 +1,31 @@
 package nl.han.ica.oose.dea.services;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class GitHubService {
 
-    public String getIndexHtml() {
-        return "";
+    public String getIndexHtml(){
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("hrrp://github.com/"))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(response -> {System.out.println(response.statusCode());
+                return response; })
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println);
+        return "re";
+
+
     }
 
     public String getReadme() {
